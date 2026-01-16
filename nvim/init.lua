f8c88c6a9d943f94d26c732c3d7cc414167376c9
nilsblix <nilsblix06@gmail.com>
2026-01-16 10:55:10 +0100

    Disable extui

--- nvim/init.lua
+++ nvim/init.lua
@@ -45,16 +45,17 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 
 -- I'm not sure I love this. Maybe I will try this in the future. It seems cool,
 -- and I do like the look of vim.o.cmdheight = 0.
-opt.cmdheight = 0
-require('vim._extui').enable({
-    enable = true, -- Whether to enable or disable the UI.
-    msg = { -- Options related to the message module.
-        ---@type 'cmd'|'msg' Where to place regular messages, either in the
-        ---cmdline or in a separate ephemeral message window.
-        target = 'msg',
-        timeout = 2000, -- Time a message is visible in the message window.
-    },
-})
+--
+-- opt.cmdheight = 0
+-- require('vim._extui').enable({
+--     enable = true, -- Whether to enable or disable the UI.
+--     msg = {        -- Options related to the message module.
+--         ---@type 'cmd'|'msg' Where to place regular messages, either in the
+--         ---cmdline or in a separate ephemeral message window.
+--         target = 'msg',
+--         timeout = 2000, -- Time a message is visible in the message window.
+--     },
+-- })
 
 require("cursor-dark").setup({
     coloured_operators = true,
