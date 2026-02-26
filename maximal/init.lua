ed4750be08d055ae8c0f7f93e72d19516c15aa74
nilsblix <nilsblix06@gmail.com>
2026-02-26 09:44:54 +0100

    use ui2

--- maximal/init.lua
+++ maximal/init.lua
@@ -57,24 +57,17 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 vim.cmd.colorscheme("github_dark_high_contrast")
--- github_dark_high_contrast has a very light statusline, and with `cmdheight = 0`
--- the constant flickering becomes unbearable, therefore having them the same colour
--- helps.
-vim.api.nvim_set_hl(0, "MsgArea", { link = "Statusline" })
 
--- I'm not sure I love this. Maybe I will try this in the future. It seems cool,
--- and I do like the look of vim.o.cmdheight = 0.
---
--- opt.cmdheight = 0
--- require('vim._extui').enable({
---     enable = true, -- Whether to enable or disable the UI.
---     msg = {        -- Options related to the message module.
---         ---@type 'cmd'|'msg' Where to place regular messages, either in the
---         ---cmdline or in a separate ephemeral message window.
---         target = 'msg',
---         timeout = 2000, -- Time a message is visible in the message window.
---     },
--- })
+opt.cmdheight = 0
+require('vim._core.ui2').enable({
+    enable = true, -- Whether to enable or disable the UI.
+    msg = {        -- Options related to the message module.
+        ---@type 'cmd'|'msg' Where to place regular messages, either in the
+        ---cmdline or in a separate ephemeral message window.
+        target = 'msg',
+        timeout = 4000, -- Time a message is visible in the message window.
+    },
+})
 
 -- =============================================================================
 --                           nvim-highlight-colors
